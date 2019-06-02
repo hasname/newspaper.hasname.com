b132b39807e234e8a7b5ef4cdf95bbe119a43993
Gea-Suan Lin <gslin@gslin.org>
2019-06-02 18:53:23 +0800

    Get necessary fields.

--- worker/spider-news.pts.org.tw.py
+++ worker/spider-news.pts.org.tw.py
@@ -16,5 +16,16 @@ def parse_url(url):
     res = requests.get(url)
     html = lxml.html.fromstring(res.text)
 
+    p = {}
+    p.content = html.cssselect('.article_content')[0].text_content()
+    p.title = html.cssselect('h2.article-title')[0].text_content()
+    p.url = url
+
+    tz = datetime.timezone(datetime.timedelta(hours=8))
+
+    claimed_str = html.cssselect('.newscont-text h2')[0].text_content()
+    ymd = re.match(r'(\d+)年(\d+)月(\d+)日', claimed_str)
+    p.claimed_at = datetime.datetime(int(ymd[1]), int(ymd[2]), int(ymd[3]), tzinfo=tz).timestamp()
+
 if '__main__' == __name__:
     main()
