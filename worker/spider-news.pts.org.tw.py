f2aceb6436918bc3b95677b241eadb00e28c3d52
Gea-Suan Lin <gslin@gslin.org>
2019-06-02 18:51:33 +0800

    Get https://news.pts.org.tw/dailynews.php and parse it.

--- worker/spider-news.pts.org.tw.py
+++ worker/spider-news.pts.org.tw.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    url = 'https://news.pts.org.tw/dailynews.php'
+    res = requests.get(url)
+
+    html = lxml.html.fromstring(res.text)
 
 if '__main__' == __name__:
     main()
