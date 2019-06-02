db0435141ea3e944543e033f04bd494ac0dcadf7
Gea-Suan Lin <gslin@gslin.org>
2019-06-02 18:52:42 +0800

    Get article links and pass into parse_url().

--- worker/spider-news.pts.org.tw.py
+++ worker/spider-news.pts.org.tw.py
@@ -8,6 +8,13 @@ def main():
     res = requests.get(url)
 
     html = lxml.html.fromstring(res.text)
+    for art in html.cssselect('a[href^="https://news.pts.org.tw/article/"]'):
+        article_url = art.get('href')
+        parse_url(article_url)
+
+def parse_url(url):
+    res = requests.get(url)
+    html = lxml.html.fromstring(res.text)
 
 if '__main__' == __name__:
     main()
